0d3cd126d2eea88376ea2deb77fbd509c1992b99
Kevin Levitz <tug97195@temple.edu>
2020-09-28 20:21:10 -0400

    Most of the logic for color change done

--- app/src/main/java/edu/temple/coloractivity/CustomAdapter.java
+++ app/src/main/java/edu/temple/coloractivity/CustomAdapter.java
@@ -1,21 +1,79 @@
 package edu.temple.coloractivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class CustomAdapter extends BaseAdapter {
-    public String[] colors;
     public Context context;
-    public LayoutInflater inflater;
+    public String[] colors = {
+            "white",
+            "blue",
+            "cyan",
+            "dkgray",
+            "gray",
+            "ltgray",
+            "magenta",
+            "green",
+            "red",
+            "yellow",
+            "black"
+    };
 
-    public void Coloradapter(Context c, String[] colors){
+    public CustomAdapter(Context c){
         this.context = c;
-        this.colors=colors;
-        inflater=(LayoutInflater.from(context));
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView textView = new TextView(context);
+        textView.setText(getItem(i).toString());
+        return textView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent){
+        View v = getView(position, convertView, parent);
+        if(((TextView) v).getText().equals("white")) {
+            v.setBackgroundColor(Color.WHITE);
+        }
+        else if(((TextView) v).getText().equals("blue")){
+            v.setBackgroundColor(Color.BLUE);
+        }
+        else if(((TextView) v).getText().equals("cyan")){
+            v.setBackgroundColor(Color.CYAN);
+        }
+        else if(((TextView) v).getText().equals("dkgray")){
+            v.setBackgroundColor(Color.DKGRAY);
+        }
+        else if(((TextView) v).getText().equals("gray")){
+            v.setBackgroundColor(Color.GRAY);
+        }
+        else if(((TextView) v).getText().equals("ltgray")){
+            v.setBackgroundColor(Color.LTGRAY);
+        }
+        else if(((TextView) v).getText().equals("magenta")){
+            v.setBackgroundColor(Color.MAGENTA);
+        }
+        else if(((TextView) v).getText().equals("green")){
+            v.setBackgroundColor(Color.GREEN);
+        }
+        else if(((TextView) v).getText().equals("red")){
+            v.setBackgroundColor(Color.RED);
+        }
+        else if(((TextView) v).getText().equals("yellow")){
+            v.setBackgroundColor(Color.YELLOW);
+        }
+        else if(((TextView) v).getText().equals("black")){
+            v.setBackgroundColor(Color.BLACK);
+        }
+        return v;
     }
 
     @Override
@@ -31,13 +89,5 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public long getItemId(int i) {
         return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        view = inflater.inflate(R.layout.activity_main, null);
-        ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        icon.setImageResource(i);
-        return view;
     }
 }
--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -2,13 +2,80 @@ package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+
+        CustomAdapter adapter = new CustomAdapter(this);
+        //Fill up Text list for colors
+        ArrayList<Object> items = new ArrayList<>();
+        for(int i = 0; i < adapter.colors.length; i++){
+            items.add(adapter.colors[i]);
+        }
+        Spinner spinner;
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        int index = (int)spinner.getSelectedItemId();
+        if(adapter.colors[index] == "white"){
+            findViewById(R.id.background).setBackgroundColor(Color.WHITE);
+        };
+        if(adapter.colors[index] == "blue"){
+            findViewById(R.id.background).setBackgroundColor(Color.BLUE);
+        };
+        if(adapter.colors[index] == "cyan"){
+            findViewById(R.id.background).setBackgroundColor(Color.CYAN);
+        };
+        if(adapter.colors[index] == "dkgray"){
+            findViewById(R.id.background).setBackgroundColor(Color.DKGRAY);
+        };
+        if(adapter.colors[index] == "gray"){
+            findViewById(R.id.background).setBackgroundColor(Color.GRAY);
+        };
+        if(adapter.colors[index] == "ltgray"){
+            findViewById(R.id.background).setBackgroundColor(Color.LTGRAY);
+        };
+        if(adapter.colors[index] == "magenta"){
+            findViewById(R.id.background).setBackgroundColor(Color.MAGENTA);
+        };
+        if(adapter.colors[index] == "green"){
+            findViewById(R.id.background).setBackgroundColor(Color.GREEN);
+        };
+        if(adapter.colors[index] == "red"){
+            findViewById(R.id.background).setBackgroundColor(Color.RED);
+        };
+        if(adapter.colors[index] == "yellow"){
+            findViewById(R.id.background).setBackgroundColor(Color.YELLOW);
+        };
+        if(adapter.colors[index] == "black"){
+            findViewById(R.id.background).setBackgroundColor(Color.BLACK);
+        };
+
+//        Context context = getApplicationContext();
+//        CharSequence text = "ID = " + spinner.getSelectedItemId();
+//        int duration = Toast.LENGTH_SHORT;
+//        Toast toast = Toast.makeText(context, text, duration);
+//        toast.show();
     }
 }
